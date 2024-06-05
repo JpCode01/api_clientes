b699bc6d11887b512d6fe62e41b80fc3325b35ed
João Pedro Vaz Garbin <joaopvgarbin@gmail.com>
2024-06-05 12:45:59 -0300

    first commit

---
+++ clientes/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from clientes.models import Cliente
+from clientes.validators import *
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf':"Número de CPFz inválido!"})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':"Inclua apenas números nesse campo!"})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg':"O RG deve ter 9 dígitos!"})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':"O número de celular deve seguir este modelo: 11 91234-1234"})
+        return data 
